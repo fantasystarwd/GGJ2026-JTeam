04fcfdae4a2e9e026fc522323481024e01fc8c0c
Wen Taichi <daleth90@gmail.com>
2026-01-31 20:07:36 +0800

    [Modify][UI] 生命條UI隨最大值調整長度

--- Assets/Scripts/UI/UIHealthBar.cs
+++ Assets/Scripts/UI/UIHealthBar.cs
@@ -1,16 +1,20 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIHealthBar : MonoBehaviour
 {
     [SerializeField]
+    private RectTransform _container;
+    [SerializeField]
     private SlicedFilledImage _fill;
     [SerializeField]
     private TMP_Text _text;
+    [SerializeField]
+    private float _widthPerValue = 2f;
 
     public void SetValue(int current, int max)
     {
+        _container.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, max * _widthPerValue);
         float fillAmount = (float)current / max;
         _fill.fillAmount = fillAmount;
         _text.text = $"{current}/{max}";
